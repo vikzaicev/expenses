882bbf8e4f91b10ef6d533fcf2d0ed5b8136edac
vikzaicev <vikzaicev@mail.ru>
2025-05-20 12:36:28 +0500

    allYear

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -4,16 +4,21 @@ import { Item } from "../Item/Item"
 import { Filter } from "../Filter/Filter"
 
 export const Cart = (props) => {
-    const [year, setYear] = useState("2025")
+    const currentYear = new Date().getFullYear()
+
+    const [year, setYear] = useState(currentYear)
 
     const yearHandler = (year) => {
         setYear(year);
     }
     const data = props.data.filter(item => item.date.includes(year))
+    const allYear = props.data.map(item => new Date(item.date).getFullYear())
+    allYear.push(currentYear)
+    const allYearSet = Array.from(new Set(allYear.sort((a, b) => a - b)));
 
     return (
         <div>
-            <Filter year={year} onYear={yearHandler} data={data} />
+            <Filter year={year} onYear={yearHandler} data={data} allYearSet={allYearSet} />
             <Item year={year} data={data} />
         </div>
     )
